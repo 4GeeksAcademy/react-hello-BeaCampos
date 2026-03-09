72fba10b6a0a83d96dc5711bab676832b0946f6a
GabyPinol <gabypinol435@gmail.com>
2026-03-09 19:15:35 +0000

    jumbotron

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Card from "./Card";
+import Jumbotron from "./Jumbotron";
 
 const Home = () => {
 	return (
 		<>
 			<Navbar />
 			<div className="container">
+				<Jumbotron 
+                    title="A Warm Welcome!" 
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid." 
+                    buttonLabel="Call to action!" 
+                />
 				<div class="row">
 					<div class="col-3">
 						<Card />
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Jumbotron = (props) => {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3 mt-4">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">
+                    {props.title}
+                </h1>
+                <p className="col-md-8 fs-4">{props.description}</p>
+                <button className="btn btn-primary btn-lg" type="button">
+                    {props.buttonLabel}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
